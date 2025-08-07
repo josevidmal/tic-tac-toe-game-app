9d740c59bbfc259f2ad88c0dfc487fb3d6c82f4c
josevidmal <92562836+josevidmal@users.noreply.github.com>
2025-08-06 21:02:37 -0600

    Deriving state, using computed value for gameBoard variable, which value is derived from the gameTurns state variable assigned to turns prop for GameBoard component

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -4,7 +4,16 @@ const initialGameBoard = [
     [null, null, null],
 ];
 
-export default function GameBoard({ onSelectSquare }) {
+export default function GameBoard({ onSelectSquare, turns }) {
+    let gameBoard = initialGameBoard;
+
+    for (const turn of turns) {
+        const { square, player } = turn;
+        const { row, col } = square;
+
+        gameBoard[row][col] = player;
+    }
+
     // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
     // function handleSelectSquare(rowIndex, colIndex) {
@@ -19,11 +28,17 @@ export default function GameBoard({ onSelectSquare }) {
 
     return (
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
-                <ol>
-                    {row.map((playerSymbol, colIndex) => <li key={colIndex}><button onClick={onSelectSquare}>{playerSymbol}</button></li>)}
-                </ol>
-            </li>)}
+            {gameBoard.map((row, rowIndex) => (
+                <li key={rowIndex}>
+                    <ol>
+                        {row.map((playerSymbol, colIndex) => (
+                            <li key={colIndex}>
+                                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                            </li>
+                        ))}
+                    </ol>
+                </li>
+            ))}
         </ol>
     );
 }
