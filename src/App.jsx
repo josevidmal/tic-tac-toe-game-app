7b701a0125fb4aa58b4dc4eeb6c353877f9956e3
josevidmal <92562836+josevidmal@users.noreply.github.com>
2025-08-07 21:33:05 -0600

    Lifting initialGameBoard and gameBoard values from GameBoard component to App.jsx. Change turns prop in GameBoard component to board prop, with gameBoard value assigned to it. Add validation logic to define a winner based on the possible winning combinations in WINNING_COMBINATIONS file

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ import GameBoard from './components/GameBoard/GameBoard.jsx';
 import Log from './components/Log/Log.jsx';
 import { WINNING_COMBINATIONS } from './winning-combinations.js';
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X';
 
@@ -20,6 +26,27 @@ function App() {
   // const [activePlayer, setActivePlayer] = useState('X');
 
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
+      winner = firstSquareSymbol;
+    }
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((currActivePlayer) => currActivePlayer === 'X' ? 'O' : 'X');
@@ -50,9 +77,10 @@ function App() {
             isActive={activePlayer === 'O'} 
           />
         </ol>
+        {winner && <p>You won, {winner}!</p>}
         <GameBoard 
           onSelectSquare={handleSelectSquare} 
-          turns={gameTurns} 
+          board={gameBoard} 
         />
       </div>
       <Log 
